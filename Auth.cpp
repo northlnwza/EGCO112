03d82b982f48b26d6860ecd1ab8beab5fe020550
tjomhongMU <thanakrit.jom@student.mahidol.edu>
2025-04-15 17:16:49 +0000

    fix auth.cpp

--- Auth.cpp
+++ Auth.cpp
@@ -5,6 +5,7 @@
 bool Auth::login(const std::string& username, const std::string& password) {
     UserList list;
 
+    list.loadFromFile("users.txt");
     return list.verifyUser(username, password);
 }
 
